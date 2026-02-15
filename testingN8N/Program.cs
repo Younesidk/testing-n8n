ed22d3fd2d4ea5c2361543b0283015b42150a9a1
Younes Lemmouchi <youneslemmouchi77@gmail.com>
2026-02-15 18:48:23 +0100

    adding another Variable Z

--- testingN8N/Program.cs
+++ testingN8N/Program.cs
@@ -1,16 +1,21 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
 
     public static void Main()
     {
-        System.Console.WriteLine();
-        System.Console.WriteLine("idk testing idk idk idk");
+        Console.WriteLine();
+        Console.WriteLine("idk testing idk idk idk");
 
         int x = 1 + 2;
-        System.Console.WriteLine(x);
+        Console.WriteLine(x);
 
         int y = x + 5;
-        System.Console.WriteLine(y);
+        Console.WriteLine(y);
+
+        int z = x + y;
+        Console.WriteLine(z);
     }
     
 }
