1c5df2f90dc3b7d47583f1b94a3bbbd413617779
Younes Lemmouchi <youneslemmouchi77@gmail.com>
2026-02-15 18:21:45 +0100

    testing

--- testingN8N/Program.cs
+++ testingN8N/Program.cs
@@ -4,7 +4,10 @@
     public static void Main()
     {
         System.Console.WriteLine();
-        System.Console.WriteLine();
+        System.Console.WriteLine("idk testing idk idk idk");
+
+        int x = 1 + 2;
+        System.Console.WriteLine(x);
     }
     
 }
